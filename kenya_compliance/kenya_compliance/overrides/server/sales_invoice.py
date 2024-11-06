59ce3e8f04834ad32809bfcac669e1ac4e1379fa
maniamartial <martialamania19@gmail.com>
2024-11-06 11:08:16 +0300

    fix - tax_type calculation on purchase invoice to take multiple similar items

--- kenya_compliance/kenya_compliance/overrides/server/sales_invoice.py
+++ kenya_compliance/kenya_compliance/overrides/server/sales_invoice.py
@@ -16,8 +16,13 @@ def on_submit(doc: Document, method: str) -> None:
 
 def before_cancel(doc: Document, method: str) -> None:
     """Disallow cancelling of submitted invoice to eTIMS."""
-    if doc.custom_successfully_submitted:
+    
+    if doc.doctype == "Sales Invoice" and doc.custom_successfully_submitted:
         frappe.throw(
-            "This invoice has already been <b>submitted</b> to eTIMS and cannot be <span style='color:red'>canceled.</span>\n"
+            "This invoice has already been <b>submitted</b> to eTIMS and cannot be <span style='color:red'>Canceled.</span>\n"
             "If you need to make adjustments, please create a Credit Note instead."
+        )
+    elif doc.doctype == "Purchase Invoice" and doc.custom_submitted_successfully:
+        frappe.throw(
+            "This invoice has already been <b>submitted</b> to eTIMS and cannot be <span style='color:red'>Canceled.</span>.\nIf you need to make adjustments, please create a Debit Note instead."
         )
